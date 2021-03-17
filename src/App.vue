824ea00222995c52b881edc9ac950aaf82bf618e
tasteofcaca <overtheskyparan.com@gmail.com>
2021-03-17 23:11:21 +0900

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <toolbar></toolbar> -->
+    <spinner :loading="loadingStatus"></spinner>
+    <tool-bar></tool-bar>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Spinner from "./components/Spinner.vue";
+import ToolBar from "./components/ToolBar.vue";
+import { bus } from "./utils/bus.js";
 export default {
-  name: 'App',
+  data() {
+    return {
+      loadingStatus: false,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ToolBar,
+    Spinner,
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    },
+  },
+  created() {
+    bus.$on("start:spinner", this.startSpinner);
+    bus.$on("end:spinner", this.endSpinner);
+  },
+  beforeDestroy() {
+    bus.$off("start:spinner", this.startSpinner);
+    bus.$off("end:spinner", this.endSpinner);
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
